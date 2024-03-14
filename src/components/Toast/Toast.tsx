190ca53754c10111e07725e1cefe386cfc4eb399
Anne L'Hôte <annelhote@users.noreply.github.com>
2024-03-14 17:24:45 +0100

    feat(toaster): Create Toaster component
    * feat(toaster): Create Toaster component
    
    * feat(toaster): Create Toaster example
    
    * fix(toaster): Add Context
    
    * fix(toaster): UseContextToaster
    
    * fix(toaster): Share ToasterType accross component and hook
    
    * fix(toaster): Set Toaster id as optional
    
    * fix(toaster): Separate types from components

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,68 @@
+import { useCallback, useEffect } from 'react';
+
+import { Container, Row } from '../Grid';
+import { Text } from '../Typography';
+import usePausableTimer from './usePausableTimer';
+
+import { ToastTypeWithId } from './types';
+
+import './styles.scss';
+
+export const Toast = (({ autoDismissAfter = 3000, description = '', id, remove = () => { }, title = '', type = 'success' }: ToastTypeWithId) => {
+  const icon = {
+    info: 'fr-icon-information-fill',
+    warning: 'fr-icon-error-warning-fill',
+    success: 'fr-icon-checkbox-circle-fill',
+    error: 'fr-icon-close-circle-fill',
+  };
+
+  const removeSelf = useCallback(() => {
+    document.getElementById(id)?.style.setProperty('animation', 'toast-unmount 1000ms');
+    setTimeout(() => {
+      remove(id);
+    }, 1000);
+  }, [id, remove]);
+
+  const { pause, resume } = usePausableTimer(removeSelf, autoDismissAfter);
+
+  useEffect(() => {
+    const progressBar = document.getElementById(`progress-${id}`);
+    if (progressBar) {
+      progressBar.style.setProperty('animation-duration', `${autoDismissAfter}ms`);
+    }
+  }, [id, autoDismissAfter]);
+
+  return (
+    <div
+      id={`${id}`}
+      role="alert"
+      className={`toast toast-${type}`}
+      onMouseEnter={pause}
+      onMouseLeave={resume}
+    >
+      <div className="toast-colored-box">
+        <span className={icon[type]} />
+        {
+          (autoDismissAfter !== 0)
+            ? (<div id={`progress-${id}`} className="toast-progress-bar" />)
+            : null
+        }
+      </div>
+      <button
+        type="button"
+        onClick={() => remove(id)}
+        className="toast-btn-close"
+      >
+        <span className='fr-icon-close-line' />
+      </button>
+      <Container fluid className="toast-content">
+        <Row>
+          {title && <Text bold className="mb-1w">{title}</Text>}
+        </Row>
+        <Row>
+          {description && (<Text className="mb-2w" size="sm">{description}</Text>)}
+        </Row>
+      </Container>
+    </div>
+  );
+});
---
+++ src/components/Toast/useToast.tsx
@@ -0,0 +1,76 @@
+import {
+  createContext,
+  PropsWithChildren,
+  useCallback,
+  useContext,
+  useId,
+  useMemo,
+  useState,
+} from 'react';
+import { createPortal } from 'react-dom';
+
+import { Toast } from './Toast';
+import { ToastType, ToastTypeWithId } from './types';
+
+type ToastContextObject = {
+  remove?: (id: string) => void,
+  toast?: (toastObject: ToastType) => string,
+  toasts?: ToastTypeWithId[],
+};
+
+type ToastProps = PropsWithChildren<ToastContextObject>;
+
+function ToastContainer({ children }: ToastProps) {
+  return <div id="toast-container">{children}</div>;
+};
+
+const ToastContext = createContext<ToastContextObject>({});
+
+// Provider
+// ==============================
+export const ToastContextProvider = ({
+  children
+}: ToastProps) => {
+  const [toasts, setToasts] = useState<ToastTypeWithId[]>([]);
+
+  const remove = useCallback((id: string) => {
+    setToasts((toastList) => toastList.filter((t) => t.id !== id));
+  }, []);
+
+  const toast = useCallback((toastObject: ToastType) => {
+    const id = toastObject?.id ?? `toast-${useId()}`;
+    setToasts((toastList) => [...toastList, { ...toastObject, id }]);
+    return id;
+  }, []);
+
+  const value: ToastContextObject = useMemo(() => ({
+    remove,
+    toast,
+    toasts,
+  }), [remove, toast, toasts]);
+
+  const content = (
+    <ToastContainer>
+      {
+        toasts.map((toast) => (
+          <Toast
+            key={toast.id}
+            remove={remove}
+            {...toast}
+          />
+        ))
+      }
+    </ToastContainer>
+  );
+
+  return (
+    <ToastContext.Provider value={value}>
+      {children}
+      {createPortal(content, document.body)}
+    </ToastContext.Provider>
+  );
+}
+
+// Hook
+// ==============================
+export const useToast = () => useContext(ToastContext);
