f4489e52a8a5b03fb5432a346e14e94069a7e7db
Anne L'Hôte <annelhote@users.noreply.github.com>
2024-03-28 09:00:09 +0100

    feat(tag-input): Add TagInput component
    * feat(tag-input): Add TagInput component
    
    * fix(ci/cd): Rebuild lib after merge
    
    * feat(tag-input): Add example
    
    * fix(tag-input): Add placeholder for text input and title for tags, accessibility
    
    * fix(tag-input): Make it accessible
    
    * fix(tag-input): Correct details
    
    * fix(tag-inpu): Do not use TextInput component
    
    * fix(tag-input): Correct class name and DOM element order
    
    * refactor(tag-input): Delete uneeded CSS
    
    * fix(tag-input): Delete unneeded custom SCSS

--- src/components/Tag/TagGroup.tsx
+++ src/components/Tag/TagGroup.tsx
@@ -1,24 +1,25 @@
 import classNames from 'classnames';
+import { useId } from 'react';
+
+import { DismissibleTag, SelectableTag, Tag } from './Tag';
 import { getChildrenOfTypes } from '../../utils/children';
 import { forwardProps } from '../../utils/props';
-import { useId } from 'react';
-import { Tag, SelectableTag, DissmissibleTag } from './Tag';
 
 export interface TagGroupProps {
-  className?: string;
   children: React.ReactNode;
+  className?: string;
 }
 
 export const TagGroup: React.FC<TagGroupProps> = ({
-  className,
   children,
+  className,
   ...props
 }) => {
   const id = useId();
   const _classes = classNames('fr-tags-group', className);
   return (
     <ul className={_classes} {...forwardProps(props)}>
-      {getChildrenOfTypes(children, [Tag, SelectableTag, DissmissibleTag]).map((child, index) => <li key={`${id}-${index}`}>{child}</li>)}
+      {getChildrenOfTypes(children, [DismissibleTag, SelectableTag, Tag]).map((child, index) => <li key={`${id}-${index}`}>{child}</li>)}
     </ul>
   );
 };
