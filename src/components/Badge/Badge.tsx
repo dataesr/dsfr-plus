225696415a7bffa3a53d67249eab7508fc2bdd91
Mihoub Debache <106969614+Mihoub2@users.noreply.github.com>
2024-07-12 15:21:17 +0200

    fix(window-start): check if window start is a function before executing (#6)
    * fix(window-start): check if window start is a function before executing
    
    * fix(window.start): check if window is not undefined
    
    * fix(badges): badges children can be anything
    
    * fix(reactNode): delete useless importation
    
    * fix(badge): children is not optionnal
    
    * fix(window.dsfr): start condition earlier
    
    * fix(window.start): check if start is a function

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -6,38 +6,45 @@ import { Link } from "../Link";
 
 const polyRef = forwardRef as PolyRefFunction;
 
-export type BadgeType =
-  | "new"
-  | 'error'
-  | 'info'
-  | 'warning'
-  | 'success';
+export type BadgeType = "new" | "error" | "info" | "warning" | "success";
 
 export type BadgeProps = {
   icon?: string;
   className?: Argument;
   size?: "md" | "sm";
-  children: string;
+  children: React.ReactNode;
   noIcon?: boolean;
   color?: DSFRColors;
   variant?: BadgeType;
-}
+};
 export const Badge = polyRef<"p", BadgeProps, OnlyAs<"button" | "p" | "a">>(
-  ({ as, className, noIcon, color = 'blue-france', size, icon, variant = 'primary', ...props }, ref) => {
-    const Component = (as === "a") ? Link : as ? as : "p";
+  (
+    {
+      as,
+      className,
+      noIcon,
+      color = "blue-france",
+      size,
+      icon,
+      variant = "primary",
+      ...props
+    },
+    ref
+  ) => {
+    const Component = as === "a" ? Link : as ? as : "p";
     const _classes = cn(
-      'fr-badge',
+      "fr-badge",
       {
         [`fr-badge--${variant}`]: variant,
         [`fr-badge--${color}`]: color,
         [`fr-icon-${icon}`]: icon,
-        // Next line is a hack to oblige dsfr to display the icon because otherwise 
+        // Next line is a hack to oblige dsfr to display the icon because otherwise
         // "content: none" is applied on .fr-badge[class^="fr-icon-"]
         "fr-badge--icon-": icon,
-        'fr-badge--no-icon': noIcon,
-        'fr-badge--sm': size === "sm",
+        "fr-badge--no-icon": noIcon,
+        "fr-badge--sm": size === "sm",
       },
-      className,
+      className
     );
     return <Component className={_classes} ref={ref} {...props} />;
   }
