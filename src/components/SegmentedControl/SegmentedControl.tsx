07abf3b4868fde86d484877d234a43b3d775e681
Anne L'Hôte <anne.lhote@gmail.com>
2026-02-12 16:41:02 +0100

    fix(segmented-control): Enable multiple segmented controls on the same page

--- src/components/SegmentedControl/SegmentedControl.tsx
+++ src/components/SegmentedControl/SegmentedControl.tsx
@@ -1,20 +1,23 @@
-import React, { useId } from 'react';
-import classNames from 'classnames';
-import { getChildrenOfType } from '../../utils/children';
-import { Merge } from '../../types/polymophic';
-import { SegmentedElement, SegmentedElementProps } from './SegmentedElement';
+import React, { useId } from "react";
+import classNames from "classnames";
+import { getChildrenOfType } from "../../utils/children";
+import { Merge } from "../../types/polymophic";
+import { SegmentedElement, SegmentedElementProps } from "./SegmentedElement";
 
 type SegmentedControlBaseProps = {
-  className?: string,
-  isVertical?: boolean,
-  label?: string,
-  legendInline?: boolean,
-  name: string,
-  onChangeValue?: (value: string) => void,
-  value?: string,
+  className?: string;
+  isVertical?: boolean;
+  label?: string;
+  legendInline?: boolean;
+  name: string;
+  onChangeValue?: (value: string) => void;
+  value?: string;
 };
 
-export type SegmentedControlProps = Merge<React.HTMLAttributes<HTMLFieldSetElement>, SegmentedControlBaseProps>;
+export type SegmentedControlProps = Merge<
+  React.HTMLAttributes<HTMLFieldSetElement>,
+  SegmentedControlBaseProps
+>;
 
 export const SegmentedControl: React.FC<SegmentedControlProps> = ({
   children,
@@ -28,45 +31,44 @@ export const SegmentedControl: React.FC<SegmentedControlProps> = ({
   ...props
 }) => {
   const id = useId();
-  const _classes = classNames('fr-segmented', className);
+  const _classes = classNames("fr-segmented", className);
   const attributes: React.HTMLAttributes<HTMLFieldSetElement> = {};
   if (isVertical) {
-    attributes.style = { display: 'block' };
+    attributes.style = { display: "block" };
   }
   return (
-    <fieldset
-      className={_classes}
-      id={id}
-      {...attributes}
-      {...props}
-    >
-      {
-        label && (
-          <legend className={classNames("fr-segmented__legend", { 'fr-segmented__legend--inline': legendInline })}>
-            {label}
-          </legend>
-        )
-      }
-      {
-        getChildrenOfType(children, SegmentedElement).map((child, index) => {
-          if (React.isValidElement(child)) {
-            return (
-              <div className={classNames("fr-segmented__elements")} key={`segmented-element-${index}`}>
+    <fieldset className={_classes} id={id} {...attributes} {...props}>
+      {label && (
+        <legend
+          className={classNames("fr-segmented__legend", {
+            "fr-segmented__legend--inline": legendInline,
+          })}
+        >
+          {label}
+        </legend>
+      )}
+      {getChildrenOfType(children, SegmentedElement).map((child, index) => {
+        if (React.isValidElement(child)) {
+          return (
+            <div
+              className={classNames("fr-segmented__elements")}
+              key={`segmented-element-${index}`}
+            >
+              {React.cloneElement(
+                child as React.ReactElement<SegmentedElementProps>,
                 {
-                  React.cloneElement(child as React.ReactElement<SegmentedElementProps>, {
-                    name,
-                    id: `${name}-${index}`,
-                    onChange: () => {
-                      onChangeValue?.(child.props.value);
-                      child?.props?.onChange?.();
-                    },
-                  })
-                }
-              </div>
-            );
-          }
-        })
-      }
+                  name,
+                  id: `${id}-${index}`,
+                  onChange: () => {
+                    onChangeValue?.(child.props.value);
+                    child?.props?.onChange?.();
+                  },
+                },
+              )}
+            </div>
+          );
+        }
+      })}
     </fieldset>
   );
 };
